f324ef93657f768a3483c601d71ba07be3835880
jose manuel gonzales trochez <joseclasef3ae@gmail.com>
2024-12-10 15:23:51 -0500

    ProductService

---
+++ NorthWind.ConsoleApp/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using NorthWind.Entities.interfaces;
+using NorthWind.Entities.ValueObjets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.ConsoleApp.Services
+{
+    internal class ProductService(IUserActionWriter writer)
+    {
+        public void Add(string user, string ProductName)
+        {
+            UserAction Action = new UserAction(user, $"Created:{ ProductName }");
+            writer.Write(Action);
+        }
+    }
+}
