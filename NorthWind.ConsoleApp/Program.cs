8d6aa0145644e72f7e2ea839b897159c384647ee
jose manuel gonzales trochez <joseclasef3ae@gmail.com>
2024-12-10 15:53:50 -0500

    DebugWriter

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -4,7 +4,10 @@ using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.interfaces;
 using NorthWind.Writers;
 
-IUserActionWriter Writer = new ConsoleWriter();
+IUserActionWriter Writer = new DebugWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application started.");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azucar refinada");
---
+++ NorthWind.Writers/DebugWriter.cs
@@ -0,0 +1,20 @@
+﻿using NorthWind.Entities.interfaces;
+using NorthWind.Entities.ValueObjets;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Writers
+{
+    public class DebugWriter : IUserActionWriter
+    {
+        public void Write(UserAction action)
+        {
+            Debug.WriteLine("ConsoleWriter: {0}, {1}, {2}",
+                        action.CreatedDataTime, action.User, action.Description);
+        }
+    }
+}
