d2bf188d5b43208db62914a4a3dc23a485f3bd2d
jose manuel gonzales trochez <joseclasef3ae@gmail.com>
2024-12-11 12:50:05 -0500

    Ejecucion por consola

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
 Builder.Services.AddSingleton<IUserActionWriter, DebugWriter>();
+Builder.Services.AddSingleton<IUserActionWriter, ConsoleWriter>();
 Builder.Services.AddSingleton<AppLogger>();
 Builder.Services.AddSingleton<ProductService>();
 using IHost AppHost = Builder.Build();
